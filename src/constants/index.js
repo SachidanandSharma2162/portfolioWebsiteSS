7c50bce76ffd9b1a763361fbafc53be3c9df3cac
sachi_2162 <sharmasachidanand1111@gmail.com>
2024-11-09 23:04:42 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,229 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    codingmafia,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    unifiedmemtor
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    // {
+    //     imageUrl: motion,
+    //     name: "Motion",
+    //     type: "Animation",
+    // },
+    // {
+    //     imageUrl: mui,
+    //     name: "Material-UI",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    // {
+    //     imageUrl: sass,
+    //     name: "Sass",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: typescript,
+    //     name: "TypeScript",
+    //     type: "Frontend",
+    // }
+];
+
+export const experiences = [
+    // {
+    //     title: "React.js Developer",
+    //     company_name: "Starbucks",
+    //     icon: starbucks,
+    //     iconBg: "#accbe1",
+    //     date: "March 2020 - April 2021",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+    // {
+    //     title: "React Native Developer",
+    //     company_name: "Tesla",
+    //     icon: tesla,
+    //     iconBg: "#fbc3bc",
+    //     date: "Jan 2021 - Feb 2022",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+    {
+        title: "Web Developer Intern",
+        company_name: "Coding Mafia",
+        icon: codingmafia,
+        iconBg: "#b7e4c7",
+        date: "June 2024 - Aug 2024",
+        points: [
+            "Developing and maintaining web applications using Java and other related technologies.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Machine Learning Intern",
+        company_name: "Unified Mentor",
+        icon: unifiedmemtor,
+        iconBg: "#a2d2ff",
+        date: "Sept 2024 - Present",
+        points: [
+            "Developing machine learning models to solve real-world problems and improve system efficiency.",
+            "Implementing and fine-tuning algorithms for supervised and unsupervised learning tasks.",
+            "Analyzing model outputs and interpreting results to make data-driven recommendations.",
+            "Conducting data preprocessing and feature engineering to enhance model performance.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/SachidanandSharma2162',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/sachidanand-sharma-728585230/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'To Do List',
+        description: 'A task management app that lets users create, organize, and prioritize their to-do lists with features like deadlines, reminders, and progress tracking.',
+        link: 'https://github.com/SachidanandSharma2162/todoList',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Portfolio Website',
+        description: 'A dynamic portfolio website showcasing projects with interactive 3D elements using Three.js, enhanced with Tailwind for styling and React.js functionality.',
+        link: 'https://github.com/SachidanandSharma2162/SS_portfolio',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Twitter Clone',
+        description: 'A Twitter clone website built using HTML, CSS, JavaScript, and a Java backend, offering core features like tweets, likes, and user authentication.',
+        link: '',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Spotify Clone',
+        description: 'A Spotify clone website built with HTML, CSS, and JavaScript, featuring a sleek user interface, music playback, and playlist management functionality..',
+        link: 'https://github.com/SachidanandSharma2162/clone_spotify',
+    },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    // }
+];
